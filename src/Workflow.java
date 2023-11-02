62307234d93ef32a956f0f3cce7ae982c951bd04
s2ma <68067942+s2ma@users.noreply.github.com>
2023-11-02 13:32:00 -0400

    Update Workflow.java

--- src/Workflow.java
+++ src/Workflow.java
@@ -1,10 +1,10 @@
 public class Workflow 
 {
     private String state;
-    private Form entry;
+    private DataEntry entry;
     public Workflow() {}
     public String getState() { return state; }
-    public Form getEntry() { return entry; }
+    public DataEntry getEntry() { return entry; }
     public void addEntry(Form entry) { this.entry = entry; }
     public void save(String update) { this.state = update; }
     public void rmEntry() { this.entry = null; }
