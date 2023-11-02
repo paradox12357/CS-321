465acc79198609b81927628f4c9ca068119a2ee0
jjoniel <jonieljerome@hotmail.com>
2023-11-02 08:58:47 -0400

    Refactoring to match class diagram

--- src/DocumentRequest.java
+++ src/DocumentRequest.java
@@ -3,6 +3,6 @@ public class DocumentRequest
     private String field;
     public DocumentRequest() {}
     public String getField() { return field; }
-    public Form getForm() { return new Form(); }
+    public DataEntry getDataEntry() { return new DataEntry(); }
     public void update(String update) { this.field = update; }
 }
