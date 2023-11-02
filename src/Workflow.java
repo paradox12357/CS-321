9f6d193a8cd2542e91281d699805d6fec30d256a
jjoniel <jonieljerome@hotmail.com>
2023-11-02 13:42:19 -0400

    Refactoring

--- src/Workflow.java
+++ src/Workflow.java
@@ -1,11 +1,11 @@
 public class Workflow 
 {
     private String state;
-    private Form entry;
+    private DataEntry entry;
     public Workflow() {}
     public String getState() { return state; }
-    public Form getEntry() { return entry; }
-    public void addEntry(Form entry) { this.entry = entry; }
+    public DataEntry getEntry() { return entry; }
+    public void addEntry(DataEntry entry) { this.entry = entry; }
     public void save(String update) { this.state = update; }
     public void rmEntry() { this.entry = null; }
 }
